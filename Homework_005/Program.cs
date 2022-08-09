d97c9f773073531d30ec19760b5cade6d5879bb0
Valery Zubkov <bouddha@mail.ru>
2022-08-09 16:51:15 +0400

    homework number 5

---
+++ Homework_005/Program.cs
@@ -0,0 +1,9 @@
+﻿// Показать числа от -N до N
+Console.WriteLine("Ведите число N: ");
+int number = int.Parse(Console.ReadLine());
+int count = -number;
+while(count <= number)
+{
+    Console.Write(" " + count + " ");
+    count++;
+}
