d9a5eff96b4625093dec73d9d32615ac2a210e05
Valery Zubkov <bouddha@mail.ru>
2022-08-09 18:13:55 +0400

    homework number 6

---
+++ Homework_006/Program.cs
@@ -0,0 +1,13 @@
+﻿// Показать последнюю цифру трёхзначного числа
+
+Console.WriteLine("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+if(number > 99 && number < 1000)
+{
+   int digitLast = number % 10;
+   Console.WriteLine("Последней цифрой введенного Вами трехзначного числа является: " + digitLast);
+}
+else
+{
+    Console.WriteLine("Вы ввели не трехзначное число. Повторите попытку.");
+}
