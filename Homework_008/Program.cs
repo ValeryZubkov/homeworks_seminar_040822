6cf1222c6c906388e2fc86e9c99b9d5f222b9c87
Valery Zubkov <bouddha@mail.ru>
2022-08-09 18:48:58 +0400

    homework number 8

---
+++ Homework_008/Program.cs
@@ -0,0 +1,20 @@
+﻿//Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+int number = new Random().Next(10, 100);
+int digitFirst = number / 10;
+int digitSecond = number % 10;
+if(digitFirst > digitSecond)
+{
+  Console.WriteLine("Наибольшей цифрой случайно выбранного числа " + number + " является: " + digitFirst);  
+}
+else
+{
+  if(digitFirst != digitSecond)
+  {
+  Console.WriteLine("Наибольшей цифрой случайно выбранного числа " + number + " является: " + digitSecond);
+  }
+  else
+  {
+    Console.WriteLine("У случайно выбранного числа " + number + " нет наибольшей цифры, т.к. они одинаковы и равны: " + digitFirst);
+  }
+}
