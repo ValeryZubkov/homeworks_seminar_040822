175343aeca4fc19bfc5b813a8d3633c4f72b12c4
Valery Zubkov <bouddha@mail.ru>
2022-08-09 19:03:56 +0400

    homework number 9

---
+++ Homework_009/Program.cs
@@ -0,0 +1,15 @@
+﻿// Удалить вторую цифру трёхзначного числа
+
+Console.WriteLine("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+if(number > 99 && number < 1000)
+{
+   int digitFirst = number / 100;
+   int digitLast = number % 10;
+   int result = digitFirst * 10 + digitLast;
+   Console.WriteLine("При удалении второй цифры трехзначного числа " + number + " получим следующее значение: " + result);
+}
+else
+{
+    Console.WriteLine("Вы ввели не трехзначное число. Повторите попытку.");
+}
