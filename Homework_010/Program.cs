15b652004a81192f0226709bd363e82f348117ca
Valery Zubkov <bouddha@mail.ru>
2022-08-09 19:43:38 +0400

    homework number 10

---
+++ Homework_010/Program.cs
@@ -0,0 +1,13 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+int numberConst = 4;
+Console.WriteLine("Введите число для проверки его кратности числу " + numberConst + ": ");
+int numberUser = int.Parse(Console.ReadLine());
+int number = numberUser % numberConst;
+if(number == 0)
+{
+    Console.WriteLine("Введенное Вами число " + numberUser + " кратно числу " + numberConst + ".");
+}
+else
+{
+    Console.WriteLine("Остаток равен " + number + " т.к. введенное Вами число " + numberUser + " не кратно числу " + numberConst + "."); 
+}
