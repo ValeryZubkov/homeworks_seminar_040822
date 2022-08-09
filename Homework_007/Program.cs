970ea83885e2fcf39d9980a185589f1100349c1a
Valery Zubkov <bouddha@mail.ru>
2022-08-09 18:23:42 +0400

    homework number 7

---
+++ Homework_007/Program.cs
@@ -0,0 +1,13 @@
+﻿//Показать вторую цифру трёхзначного числа
+
+Console.WriteLine("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+if(number > 99 && number < 1000)
+{
+   int digitLast = number % 100 / 10;
+   Console.WriteLine("Второй цифрой введенного Вами трехзначного числа является: " + digitLast);
+}
+else
+{
+    Console.WriteLine("Вы ввели не трехзначное число. Повторите попытку.");
+}
