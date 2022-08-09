c74b45cb74ccb44e875fa118aad47824a3290c01
Valery Zubkov <bouddha@mail.ru>
2022-08-09 16:27:30 +0400

    homework number 4

---
+++ Homework_004/Program.cs
@@ -0,0 +1,12 @@
+﻿// Выяснить является ли число чётным
+
+Console.WriteLine("Ведите число для проверки: ");
+int number = int.Parse(Console.ReadLine());
+if(number%2 == 0)
+{
+  Console.WriteLine("Введенное Вами число " + number+ " яляется четным!");
+}
+else
+{
+  Console.WriteLine("К сожалению, введенное Вами число " + number+ " не яляется четным.");  
+}
