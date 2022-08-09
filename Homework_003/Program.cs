628a16e46ae1559feb3f0879990135f4b9e4573f
Valery Zubkov <bouddha@mail.ru>
2022-08-09 16:14:45 +0400

    homework number 3

---
+++ Homework_003/Program.cs
@@ -0,0 +1,8 @@
+﻿// Найти максимальное из трех чисел
+int a = 31;
+int b = 354;
+int c = 918;
+int max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+Console.WriteLine("Максимальное число равно: " + max);
